8f65b85e75140880ff659ae915835b5e1ff9a965
Liang Aoi <aoi010122@becreator.com.tw>
2023-05-25 13:37:25 +0800

    新增搜尋功能 & 篩選功能

--- src/pages/prodcutPage/prodcutPage.vue
+++ src/pages/prodcutPage/prodcutPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, reactive, provide } from 'vue'
+import { ref, onMounted, reactive, provide, watch } from 'vue'
 import prodcutPage_coupon from './prodcutPage_coupon.vue';
 import prodcutPage_category from './prodcutPage_category.vue';
 import pagination from '@COM/pagination/pagination.vue';
@@ -7,9 +7,11 @@ import Service from "@SERVICE/service";
 import { useStore } from '@STORE/main';
 
 const store = useStore();
+const searchText = ref('')
 const page = ref(1)
 const paginationValue = ref(50)
 const category = ref([])
+const isSort = ref('asc')
 const ProductDataList = reactive({
   data: {},
 });
@@ -35,10 +37,11 @@ const postProductFilter = async () => {
 const onPageChange = (val) => {
   page.value = val
   let submitData = {
-    searchText: '',
+    searchText: searchText.value,
     category: category.value,
     page: val,
     pagination: paginationValue.value,
+    isSort: isSort.value,
   };
   postProductDatabase(submitData)
 }
@@ -47,10 +50,26 @@ const callCategoryFilter = (value) => {
   category.value = [value]
   page.value = 1
   let submitData = {
-    searchText: '',
+    searchText: searchText.value,
     category: [value],
     page: page.value,
     pagination: paginationValue.value,
+    isSort: isSort.value,
+  };
+  postProductDatabase(submitData)
+};
+
+const callPriceFilter = () => {
+  isSort.value = isSort.value === 'asc' ? 'desc' : 'asc';
+};
+
+const callSearch = () => {
+  let submitData = {
+    searchText: searchText.value,
+    category: category.value,
+    page: page.value,
+    pagination: paginationValue.value,
+    isSort: isSort.value,
   };
   postProductDatabase(submitData)
 };
@@ -61,8 +80,19 @@ onMounted(() => {
     category: [],
     page: page.value,
     pagination: paginationValue.value,
+    isSort: isSort.value,
   })
   postProductFilter()
+});
+
+watch(isSort, (newVal, oldVal) => {
+  postProductDatabase({
+    searchText: searchText.value,
+    category: category.value,
+    page: page.value,
+    pagination: paginationValue.value,
+    isSort: isSort.value,
+  })
 });
 
 provide('categoryList', categoryList);
@@ -75,7 +105,23 @@ provide('callCategoryFilter', callCategoryFilter);
   <div class="prodcutPage_container">
     <div>
       <div class="prodcutPage_category">
-        <prodcutPage_category />
+        <button style="flex: 2.5;" class="prodcutPage_category_container" @click="callPriceFilter()">
+          <img class="prodcutPage_category_img" src="../../assets/filter.png" />
+          <template v-if="isSort === 'asc'">
+            <a>価格が低い順</a>
+          </template>
+          <template v-else>
+            <a>価格が高い順</a>
+          </template>
+        </button>
+        <div style="flex: 5;" class="prodcutPage_category_container">
+          <prodcutPage_category />
+        </div>
+        <div style="flex: 2.5;" class="prodcutPage_category_container">
+          <input type="text" v-model="searchText" @keydown.enter="callSearch" />
+          <button @click="callSearch"><img class="prodcutPage_category_img" src="../../assets/search.png" /></button>
+        </div>
+
       </div>
       <div class="prodcutPage_Item">
         <template v-if="ProductDataList?.data?.length > 0" v-for="(item, index) in ProductDataList.data">
