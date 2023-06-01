2dfcb4e2357d76c724b0fcf9e7835f22f379d789
Liang Aoi <aoi010122@becreator.com.tw>
2023-06-01 16:29:34 +0800

    導覽頁更新至置底

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,7 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 import headerbar from '@COM/header-bar/headerBarPage.vue';
-import footerSide from '@COM/footerSide/footerSidePage.vue';
 import navBugerPage from '@COM/nav-buger/navBugerPage.vue';
-import asideMenuPage from '@COM/aside-menu/asideMenuPage.vue';
 
 const isView = ref(false)
 const handViewChange = () => {
@@ -18,18 +16,14 @@ const router = useRouter()
 
 <template>
   <div class="container">
+    <navBugerPage v-if="router.currentRoute.value.path !== ('/loginPage')" :isView="isView"
+      @handViewChange="handViewChange()" />
     <div class="content">
-      <headerbar v-if="router.currentRoute.value.path !== ('/loginPage')" class="head" />
-      <navBugerPage v-if="router.currentRoute.value.path !== ('/loginPage')" :isView="isView"
-        @handViewChange="handViewChange()" />
-      <transition :class="{ asideMenuPageTransform: isView }">
-        <asideMenuPage class="asideMenuPage" :isView="isView" @handViewChange="handViewChange" />
-      </transition>
       <div class="view">
         <router-view />
       </div>
     </div>
-    <footerSide v-if="router.currentRoute.value.path !== ('/loginPage')" class="footer" />
+    <headerbar v-if="router.currentRoute.value.path !== ('/loginPage')" class="head" />
   </div>
 </template>
 
