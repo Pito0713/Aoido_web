b6fcaae53ab74077d42f1e8f824cd0e16a8c64a9
Liang Aoi <aoi010122@becreator.com.tw>
2023-06-02 17:30:04 +0800

    修改路徑css

--- src/components/header-bar/headerBarPage.vue
+++ src/components/header-bar/headerBarPage.vue
@@ -1,14 +1,21 @@
 <script setup>
 import { MENU_LIST } from '../../configs/site'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
 </script>
 
 <template>
   <div>
     <ul class="nav_branch">
       <li v-for="(item, index) in MENU_LIST">
-        <router-link class="nav_items" :to="item.route" :key="index">
-          <img class="nav_items_img" :src=item.img alt="" />
-          <a class="nav_items_text"> {{ item.text }}</a>
+        <router-link class="nav_items" :to="item.route" :key="index"
+          :class="{ nav_items_animation: router.currentRoute.value.fullPath === item.route }">
+          <img class="nav_items_img" :src=item.img alt=""
+            :class="{ nav_items_img_animation: router.currentRoute.value.fullPath === item.route }" />
+          <a class="nav_items_text"
+            :class="{ nav_items_text_animation: router.currentRoute.value.fullPath === item.route }">
+            {{ item.text }}</a>
         </router-link>
       </li>
     </ul>
